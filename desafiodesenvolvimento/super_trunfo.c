cbefc33b8218e5e722fbec42ac997a3b11eee401
Desenvolvimentohub <holcilene@gmail.com>
2025-04-04 00:07:04 +0000

    commit 1

--- desafiodesenvolvimento/super_trunfo.c
+++ desafiodesenvolvimento/super_trunfo.c
@@ -1,25 +1,24 @@
 #include <stdio.h>;
 
 int main(){
-    printf("Desafio supertrunfo - carta1" \n);
+
+    printf("Desafio supertrunfo - carta1\n");
     char Estado = "Bahia";
-    char codigo [10] = 'S01'
-    char cidade[50] = "Salvador";
-    int populacao = 
-    float area;
-    float PIB;
-    int pontosturisticos = 30
+    char codigo [10] = 'S01';
+    char cidade [20] = "Salvador";
+    int populacao = 6748000;
+    float area = 1200.25;
+    float PIB =  300.50;
+    int turisticos = 25;
 
 
-    printf("Desafio supertrunfo - carta2" \n);
+    printf("carta2\n");
     char Estado = "Ceara";
-    char codigo [10] = 'C01'
-    char cidade[50] = "Fortlaeza";
-    int populacao = 
-    float area;
-    float PIB;
-    int pontosturisticos = 30
-
-
-
+    char codigo [10] = 'C01';
+    char cidade [50] = "Fortaleza";
+    int populacao = 12325000;
+    float area = 152111;
+    float PIB = 699.28;
+    int turisticos = 30;
+ 
 }

e095c535f9745e77a71ef29466e0bb9c4286184f
Desenvolvimentohub <holcilene@gmail.com>
2025-04-04 23:12:01 +0000

    printf

--- desafiodesenvolvimento/super_trunfo.c
+++ desafiodesenvolvimento/super_trunfo.c
@@ -2,25 +2,53 @@
 
 int main(){
 
-    printf("Desafio supertrunfo - carta1\n");
-    char Estado = "Bahia";
-    char codigo [10] = 'S01';
-    char cidade [20] = "Salvador";
-    int populacao = 6748000;
-    float area = 1200.25;
-    float PIB =  300.50;
-    int turisticos = 25;
+    printf("Desafio supertrunfo - carta01\n");
+    printf("\n");
 
-    printf("Codigo da cidade: %s\n", codigo);
+    char Estado01 [15]= "Bahia";
+    char codigo01 [20] = "B01";
+    char cidade01 [20] = "Salvador";
+    int populacao01 = 6748000;
+    float area01 = 120025;
+    float PIB01 =  300.50;
+    int turisticos01 = 25;
+
+    printf("Codigo: %s\n", codigo01);
+    printf("Nome do Estado: %s \n", Estado01);
+    printf("Cidade: %s \n", cidade01);
+    printf("Populacao: %d \n", populacao01);
+    printf("Area(km²): %f \n", area01);
+    printf("PIB(bilhoes): %f \n",PIB01);
+    printf("Pontos_turisticos: %d \n", turisticos01);
+    
+    printf("\n");
+    printf("\n");
 
 
-    printf("carta2\n");
-    char Estado = "Ceara";
-    char codigo [10] = 'C01';
-    char cidade [50] = "Fortaleza";
-    int populacao = 12325000;
-    float area = 152111;
-    float PIB = 699.28;
-    int turisticos = 30;
- 
+
+    printf("carta02\n");
+    printf("\n");
+    char Estado02 [20]= "Ceara";
+    char codigo02 [20]= "C01";
+    char cidade02 [50] = "Fortaleza";
+    int populacao02 = 12325000;
+    float area02 = 152111;
+    float PIB02 = 699.28;
+    int turisticos02 = 30;
+
+
+
+    printf("Codigo: %s\n", codigo02);
+    printf("Nome do Estado: %s \n", Estado02);
+    printf("Cidade: %s \n", cidade02);
+    printf("Populacao: %d \n", populacao02);
+    printf("Area(km²): %f \n", area02);
+    printf("PIB(bilhoes): %f \n",PIB02);
+    printf("Pontos_turisticos: %d \n", turisticos02);
+
+    return 0;
+
+
 }
+
+    
